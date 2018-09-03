62bc04cb60ac21d85c575ab21e87a7163ab1e274
Empty <570412454@qq.com>
2018-09-03 19:26:49 +0800

    Update vpnc.rb
    删除

--- Formula/vpnc.rb
+++ Formula/vpnc.rb
@@ -5,10 +5,11 @@ class Vpnc < Formula
   mirror "https://mirrors.kernel.org/debian/pool/main/v/vpnc/vpnc_0.5.3r550.orig.tar.gz"
   version "0.5.3r550"
   sha256 "a6afdd55db20e2c17b3e1ea9e3f017894111ec4ad94622644fc841c146942e71"
-
+  revision 1
 
   bottle do
     cellar :any
+    revision 1
     sha256 "f9dcd6133700d6a752a980cf1547270f03e188666db826764c087b5ce7026b4f" => :mavericks
   end
 
@@ -22,9 +23,7 @@ class Vpnc < Formula
   depends_on "gnutls"
   depends_on :tuntap
   depends_on "openssl" if build.with? "hybrid"
-
-  fails_with :llvm do
-    build 2334
+	
   end
 
   # Patch from user @Imagesafari to enable compilation on Lion
@@ -66,35 +65,3 @@ class Vpnc < Formula
 end
 
 __END__
---- vpnc/sysdep.h	2008-11-19 15:36:12.000000000 -0500
-+++ vpnc.patched/sysdep.h	2011-07-14 12:49:18.000000000 -0400
-@@ -109,6 +109,8 @@
- #define HAVE_FGETLN    1
- #define HAVE_UNSETENV  1
- #define HAVE_SETENV    1
-+#define HAVE_GETLINE   1
-+#define NEW_TUN        1
- #endif
- 
- /***************************************************************************/
-diff -u vpnc.patched/vpnc-script vpnc/vpnc-script
---- vpnc.patched/vpnc-script	2013-11-01 13:17:21.000000000 -0700
-+++ vpnc/vpnc-script	2013-11-01 18:54:33.000000000 -0700
-@@ -388,7 +388,7 @@
-				scutil >/dev/null 2>&1 <<-EOF
-					open
-					d.init
--					d.add ServerAddresses * $INTERNAL_IP4_DNS
-+					d.add ServerAddresses * $INTERNAL_IP4_DNS $INTERNAL_IP6_DNS
-					set State:/Network/Service/$TUNDEV/DNS
-					d.init
-					# next line overrides the default gateway and breaks split routing
-@@ -598,7 +598,7 @@
-			fi
-			i=`expr $i + 1`
-		done
--		for i in $INTERNAL_IP4_DNS ; do
-+		for i in $INTERNAL_IP6_DNS ; do
-			if echo "$i" | grep : >/dev/null; then
-				set_ipv6_network_route "$i" "128"
-			fi
